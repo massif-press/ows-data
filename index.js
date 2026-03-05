5fbb162d6ec5db19c817ba4e29e675294cd58a8c
J. Arena <john.arena.3@gmail.com>
2026-03-05 16:03:02 -0600

    more v3 updates

--- index.js
+++ index.js
@@ -2,23 +2,13 @@
 
 const data = {
   lcp_manifest: require('./lib/lcp_manifest.json'),
-  // actions: require('./lib/actions.json'),
-  // backgrounds: require('./lib/backgrounds.json'),
   core_bonuses: require('./lib/core_bonuses.json'),
-  // environments: require('./lib/environments.json'),
   frames: require('./lib/frames.json'),
-  // manufacturers: require('./lib/manufacturers.json'),
   mods: require('./lib/mods.json'),
-  // pilot_gear: require('./lib/pilot_gear.json'),
-  // reserves: require('./lib/reserves.json'),
-  // sitreps: require('./lib/sitreps.json'),
-  // skills: require('./lib/skills.json'),
-  // statuses: require('./lib/statuses.json'),
   systems: require('./lib/systems.json'),
-  // tables: require('./lib/tables.json'),
-  // tags: require('./lib/tags.json'),
-  // talents: require('./lib/talents.json'),
   weapons: require('./lib/weapons.json'),
+  npcc_mbt: require('./lib/npcc_mbt.json'),
+  npct_marine: require('./lib/npct_marine.json'),
 };
 
 module.exports = data;
